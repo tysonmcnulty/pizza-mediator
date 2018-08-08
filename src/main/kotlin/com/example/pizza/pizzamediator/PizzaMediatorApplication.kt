46ec2826d5c3753a54abefb4f0e80e85b974c8f1
Tyson McNulty <tyson.mcnulty@gmail.com>
2018-08-08 08:33:48 -0400

    First commit - Spring/Kotlin app

---
+++ src/main/kotlin/com/example/pizza/pizzamediator/PizzaMediatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pizza.pizzamediator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PizzaMediatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<PizzaMediatorApplication>(*args)
+}
